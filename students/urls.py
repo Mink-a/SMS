3e877b836ba4c1a46cebb4b8e99f1b91c65cb96a
Mink-a <rahulakhant@gmail.com>
2022-10-05 13:48:14 +0630

    crud done

--- students/urls.py
+++ students/urls.py
@@ -2,5 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('<int:id>', views.view_student, name='view_student'),
+    path('add/', views.add, name='add_student'),
+    path('update/<int:id>', views.update, name='update_student'),
+    path('delete/<int:id>', views.delete_student, name='delete_student'),
 ]
